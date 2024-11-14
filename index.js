4e652069ad0e2ac974ec419cbcab2293371dfc1e
msantiagob <miguelsant2001@gmail.com>
2024-11-14 03:22:37 -0500

    add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     // Manejar las solicitudes OPTIONS
